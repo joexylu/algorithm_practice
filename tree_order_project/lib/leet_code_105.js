bea463e5ce85ada8f852c67f2f41a09293391082
joexylu <joe.luxiyang@gmail.com>
2020-05-18 17:12:35 -0700

    Construct Binary Tree from Preorder and Inorder Traversal

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,24 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+    //base case
+    if(preorder.length === 0 && inorder.length === 0) return null;
+    //find rootNode
+    let rootVal = preorder[0];
+    let root = new TreeNode(rootVal);
+    //find inorder
+    let midIndex = inorder.indexOf(rootVal)
+    let leftInorder = inorder.slice(0,midIndex);
+    let rightInorder = inorder.slice(midIndex+1)
 
+    //find preorder
+    let leftPreorder = preorder.filter(val => leftInorder.includes(val))
+    let rightPreorder = preorder.filter(val => rightInorder.includes(val))
+
+    let left = buildTree(leftPreorder, leftInorder);
+    let right = buildTree(rightPreorder, rightInorder);
+
+    root.left = left;
+    root.right = right;
+    return root;
 }
