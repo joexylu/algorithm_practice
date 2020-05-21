9b86bfc13b2e90f469e80de7fd49ba9d545bb50d
joexylu <joe.luxiyang@gmail.com>
2020-05-20 22:39:28 -0700

    DBF project

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,13 @@
 function breadthFirstArray(root) {
-
+    let queue = [root]
+    const result = []
+    while (queue.length){
+        let node = queue.shift();
+        result.push(node.val)
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return result
 }
 
 module.exports = {
--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,20 @@
 function treeSum(root) {
+    // if(!root) return 0;
 
+    // let queue = [root]
+    // let result = 0
+    // while (queue.length){
+    //     let node = queue.shift();
+    //     result += node.val
+    //     if (node.left) queue.push(node.left);
+    //     if (node.right) queue.push(node.right);
+    // }
+    // return result
+
+
+
+    if(!root) return 0;
+    return treeSum(root.left) + root.val + treeSum(root.right)
 }
 
 
