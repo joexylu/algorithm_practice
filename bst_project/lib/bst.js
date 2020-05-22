85f976bdc55f64bf42abc6ef5765fa087557c4b5
joexylu <joe.luxiyang@gmail.com>
2020-05-21 18:16:40 -0700

    bst set up

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -8,7 +8,61 @@ class TreeNode {
 
 
 class BST {
-   
+   constructor(){
+       this.root = null;
+       this.stack = []
+   }
+
+   insert(val, root=this.root){
+        if(this.root === null) {
+            this.root = new TreeNode(val);
+            return;
+        }
+
+        if(val < root.val){
+            if(!root.left){
+                root.left = new TreeNode(val);
+            }else{
+                this.insert(val, root.left)
+            }
+        }else{
+            if(!root.right){
+                root.right = new TreeNode(val)
+            } else{
+                this.insert(val, root.right)
+            }
+        }
+   }
+
+   inOrderPrint(root=this.root){
+       if(!root) return;
+       this.inOrderPrint(root.left);
+       this.stack.push(root.val)
+       this.inOrderPrint(root.right);
+   }
+
+   searchRecur(val, root=this.root){
+        if (!root) return false;
+        if (val < root.val) {
+            return this.searchRecur(val, root.left);
+        } else if (val > root.val){
+            return this.searchRecur(val, root.right);
+        } else {
+            return true;
+        }
+   }
+
+   searchIter(val, root=this.root){
+        this.inOrderPrint(root)
+        let i = 0;
+        while( i < this.stack.length){
+            if(this.stack[i] === val){
+                return true
+            }
+            i++;
+        }
+        return false
+   }
 }
 
 module.exports = {
