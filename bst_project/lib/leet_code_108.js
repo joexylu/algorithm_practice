53ca52820c3857f395d280a2b6740a9f80a6bcf0
joexylu <joe.luxiyang@gmail.com>
2020-05-21 18:43:26 -0700

    bst leed code

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -3,5 +3,10 @@
 
 
 function sortedArrayToBST(nums) {
-
+    if (nums.length === 0) return null;
+    let mid = Math.floor(nums.length/2);
+    let root = new TreeNode(nums[mid])
+    root.left = sortedArrayToBST(nums.slice(0,mid))
+    root.right = sortedArrayToBST(nums.slice(mid+1))
+    return root
 }
--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -3,5 +3,18 @@
 
 
 function isBalanced(root) {
+    if (!root) return true;
+    let left, right
+    
+    left = treeHeight(root.left);
+    right = treeHeight(root.right);
+
+    return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+}
+
+function treeHeight(root) {
+    if(!root) return -1;
+
+    return 1 + Math.max(treeHeight(root.left), treeHeight(root.right))
 
 }
