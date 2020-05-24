839609bc79741d44e586a16e479bc5947efb78dc
joexylu <joe.luxiyang@gmail.com>
2020-05-23 17:26:14 -0700

    course problem

--- graph_project/lib/leet_code_207.js
+++ graph_project/lib/leet_code_207.js
@@ -1,6 +1,42 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/course-schedule/
 
-function canFinish(numCourses, prerequisites) {
 
+function buildeGraph(list){
+    let graph = {}
+    list.forEach(prereq => {
+        let [course, pre] = prereq.map(String);
+        if(!graph[course]){
+            graph[course] = [pre]
+        }else{
+            graph[course].push(pre)
+        }
+
+        //if course has no prereq 
+        if(!graph[pre]){
+            graph[pre] = []
+        }
+    });
+    return graph
+}
+
+function canFinish(numCourses, prerequisites) {
+    let graph = buildeGraph(prerequisites);
+    let total = Object.keys(graph).length
+    let completed = new Set()
+
+    let taking = true
+
+    while(taking){
+        taking = false
+        for(let course in graph){
+            let finishpre = graph[course].every(pre => completed.has(pre));
+            if(!completed.has(course) && finishpre){
+                completed.add(course)
+                taking = true
+            }
+        }
+    }
+
+    return completed.size === total
 }
