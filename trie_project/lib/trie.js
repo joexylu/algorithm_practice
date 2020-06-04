f9569b5830e6f8764be775d40b33b19f51261dfb
joexylu <joe.luxiyang@gmail.com>
2020-06-03 22:53:05 -0700

    tries recur

--- trie_project/lib/trie.js
+++ trie_project/lib/trie.js
@@ -1,9 +1,54 @@
 class Node {
-
+    constructor() {
+        this.children = {};
+        this.isTerminal = false;
+    }
 }
 
 class Trie {
-   
+    constructor() {
+        this.root = new Node();
+    }
+
+    insertRecur(word, root=this.root){
+        let letter = word[0]
+
+        if(!(letter in root.children)){
+            root.children[letter] = new Node()
+        }
+
+        if(word.length === 1){
+            root.children[letter].isTerminal = true;
+        }else{
+            this.insertRecur(word.slice(1), root.children[letter])
+        }
+    }
+
+    insertIter(word, root=this.root){
+
+    }
+
+    searchRecur(word, root=this.root){
+        if(word.length === 0){
+            if(root.isTerminal){
+                return true
+            }else{
+                return false
+            }
+        }
+
+        let letter = word[0]
+
+        if(letter in root.children){
+            return this.searchRecur(word.slice(1), root.children[letter])
+        }else{
+            return false
+        }
+    }
+
+    searchIter(word, root=this.root){
+
+    }
 }
 
 module.exports = {
