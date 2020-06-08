16438d58c8f97c4ba57eabae0410af9a5a67ffeb
joexylu <joe.luxiyang@gmail.com>
2020-06-08 15:10:30 -0700

    small-diff

--- AlgoExpert/Arr/mid-smallest-dif.js
+++ AlgoExpert/Arr/mid-smallest-dif.js
@@ -1,6 +1,37 @@
 function smallestDifference(arrayOne, arrayTwo) {
     // Write your code here.
 
-    
+    arrayOne.sort((a,b)=> a-b);
+    arrayTwo.sort((a,b)=> a-b);
+
+    let idxOne = 0;
+    let idxTwo = 0;
+
+    let small = Infinity;
+    let currentDiff;
+    let smallestPair;
+
+    while(idxOne < arrayOne.length && idxTwo < arrayTwo.length){
+
+        firstNum = arrayOne[idxOne];
+        secondNum = arrayTwo[idxTwo];
+
+        if(firstNum < secondNum){
+            currentDiff = secondNum - firstNum
+            idxOne += 1
+        }else if(secondNum < firstNum){
+            currentDiff = firstNum - secondNum
+            idxTwo += 1
+        }else{
+            return [firstNum, secondNum]
+        }
+
+        if(small > currentDiff){
+            small = currentDiff;
+            smallestPair = [firstNum, secondNum]
+        }
+    }
+
+    return smallestPair
 }
   
