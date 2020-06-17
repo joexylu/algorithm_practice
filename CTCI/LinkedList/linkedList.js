f973db22ef984252b7792870bd18c196afe53b82
joexylu <joe.luxiyang@gmail.com>
2020-06-16 22:10:33 -0700

    linked list apply

---
+++ CTCI/LinkedList/linkedList.js
@@ -0,0 +1,84 @@
+class Node{
+    constructor(val){
+        this.value = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    addToTail(val){
+        let node  = new Node(val)
+
+        if(this.tail){
+            this.tail.next = node;
+        }else{
+            this.head = node
+        }
+
+        this.tail = node;
+        this.length += 1;
+        return this
+    }
+
+    removeTail(){
+        if(!this.tail){
+            return undefined
+        }else{
+
+            // iterate through all the node:
+            let current = this.head;
+            let prev = current;
+            while(current.next){
+                prev = current;
+                current = current.next;
+            }
+
+            let removed = this.tail;
+            this.tail = prev;
+            this.tail.next = null;
+            this.length -= 1;
+
+            if(this.length === 0){
+                this.head = null;
+                this.tail = null;
+            }
+            return removed
+        }
+    }
+
+    addToHead(val){
+        let node = new Node(val);
+
+        if(this.head){
+            node.next = this.head;
+        }else{
+            this.tail = node
+        }
+        this.head = node;
+        this.length += 1;
+        return this;
+    }
+
+    removeHead(){
+        if(!this.head){
+            return undefined
+        }else{
+            let removed = this.head
+            this.head = this.head.next;
+            this.length -= 1;
+
+            if(this.length === 0){
+                this.head = null;
+                this.tail = null
+            }
+            return removed
+        }
+    }
+}
