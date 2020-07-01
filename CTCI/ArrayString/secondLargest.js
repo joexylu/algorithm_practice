b33fa3608961d002e72cf3122a1942e3f42258ae
joexylu <joe.luxiyang@gmail.com>
2020-06-30 22:49:49 -0700

    second largest

---
+++ CTCI/ArrayString/secondLargest.js
@@ -0,0 +1,15 @@
+function nextBiggest(arr) {
+    let max = -Infinity, result = -Infinity;
+  
+    for (const value of arr) {
+      const nr = Number(value)
+  
+      if (nr > max) {
+        [result, max] = [max, nr] // save previous max
+      } else if (nr < max && nr > result) {
+        result = nr; // new second biggest
+      }
+    }
+  
+    return result;
+  }
