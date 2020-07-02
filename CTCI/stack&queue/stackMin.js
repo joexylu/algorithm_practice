cee410a193317dfe252266137afbeb0337a6fbbf
joexylu <joe.luxiyang@gmail.com>
2020-07-02 16:21:02 -0700

    stack min

--- CTCI/stack&queue/stackMin.js
+++ CTCI/stack&queue/stackMin.js
@@ -1,13 +1,46 @@
 class StackMin{
     constructor(){
-        this.data = []
+        this.data = [];
+        this.allMins = [];
         this.min = -Infinity;
         this.length = 0;
         this.top
     }
 
-    push(num){
+    pushToStack(num){
+        if(this.length === 0){
+            this.data.push(num);
+            this.min = num
+            this.allMins.push(this.min)
+        }else{
+            if(num < this.min){
+                this.min = num
+                this.allMins.push(this.min)
+            }
+            this.data.push(num)
+        }
+        this.top = this.data[-1];
+        this.length ++
+    }
 
+    popToStack(){
+        if(this.length === 0){
+            return false
+        }
+
+        let popNum = this.data.pop();
+        if(popNum === this.min){
+            this.allMins.pop();
+            this.min = this.allMins[-1]
+        }
+
+        this.length--
+
+        return popNum
+    }
+
+    peek(){
+        return this.data[-1]
     }
 }
 
