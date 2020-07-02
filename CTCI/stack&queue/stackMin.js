1f8248c3ffe2912d61b5a4943b605591930033e0
joexylu <joe.luxiyang@gmail.com>
2020-07-02 16:06:24 -0700

    three in one stack naive way

---
+++ CTCI/stack&queue/stackMin.js
@@ -0,0 +1,13 @@
+class StackMin{
+    constructor(){
+        this.data = []
+        this.min = -Infinity;
+        this.length = 0;
+        this.top
+    }
+
+    push(num){
+
+    }
+}
+
--- CTCI/stack&queue/threeStackInOne.js
+++ CTCI/stack&queue/threeStackInOne.js
@@ -1,7 +1,29 @@
 //three stack in one
 
-function threeStackInOne(){
-    
+// first stack 0, 3, 6, 9; second 1,4,7,10; third 2, 5,8,11
+class NStack{
+    constructor(n){
+        this.stacks = [];
+        this.size = new Array(n).fill(0)
+    }
+
+
+    peek(i){
+        return this.stack[i+(this.size[i]-1)*this.size.length]
+    }
+
+    pop(i){
+        if(this.size[i]> 0){
+            return this.stack.splice(i+(--this.size[i] * this.size.length),1)
+        }
+    }
+
+    push(i, num){
+        if(this.size[i] >= 0){
+            this.stack[i+((this.size[i]++) * this.size.length)] = num
+        }
+    }
+
 }
 
 
