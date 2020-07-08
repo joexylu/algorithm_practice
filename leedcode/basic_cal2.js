fc0b063c180d9a0589b793807deb655ef28ba497
joexylu <joe.luxiyang@gmail.com>
2020-07-07 23:35:21 -0700

    leedproblem cal

---
+++ leedcode/basic_cal2.js
@@ -0,0 +1,25 @@
+var calculate = function(s) {
+    s.replace(/\s/g, '');
+    let num = ""
+    let calc = [];
+    let prevSign = '+'
+    for (let i=0;i<s.length;i++) {
+        if (!isNaN(s[i])) {
+            num+=s[i];
+        }
+        if (isNaN(s[i]) || i == s.length-1) {
+            if (prevSign == "+") {
+                calc.push(Number(num));
+            } else if (prevSign == "-") {
+                calc.push(Number(-num));
+            } else if (prevSign == "*") {
+                calc.push(Math.floor(calc.pop()*num));
+            } else {
+                calc.push(Math.trunc(calc.pop()/num))
+            }
+            prevSign = s[i];
+            num="";
+        }
+    }
+    return calc.reduce((a,b)=>a+b);
+};
