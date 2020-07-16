23833f44404888c9c5540835cb57eb0311e5fbed
joexylu <joe.luxiyang@gmail.com>
2020-07-15 17:12:43 -0700

    first inital ood call center

---
+++ CTCI/OOD/callCenter.js
@@ -0,0 +1,35 @@
+//all three ranks of employees have different work to be done.
+//
+//finally, call handle class would route call to correct person
+
+
+class CallHandler{
+    constructor(){
+        this.numRespondents = 10;
+        this.numManagers = 4;
+        this.numDirector = 2;
+
+        this.employeeLevel = ["respondents", "managers", "directors"]
+
+        this.callQuere = [];
+
+    }
+
+    getHandlerCall(call){
+
+    }
+
+    dispatchCall(caller){
+        let call = new CallHandler(caller)
+        dispatchCall(call)
+    }
+
+
+
+
+}
+
+
+class Call{
+    constructor(caller)
+}
