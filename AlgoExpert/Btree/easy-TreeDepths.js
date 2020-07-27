82ab8d8b14dcc2b8a45a6f21cc633f879d383d17
joexylu <joe.luxiyang@gmail.com>
2020-07-26 18:20:49 -0700

    solve node depth

--- AlgoExpert/Btree/easy-TreeDepths.js
+++ AlgoExpert/Btree/easy-TreeDepths.js
@@ -5,3 +5,26 @@ class BinaryTree{
         this.right = null;
     }
 }
+
+function nodeDepths(root, sum = 0){
+    if(!root) return 0;
+
+    return sum + nodeDepths(root.left, sum + 1) + nodeDepths(root.right, sum + 1)
+}
+
+
+// function nodeDepthIter(root){
+//     let sumDepth = 0;
+//     let stack = [{node: root, depth: 0}];
+
+//     while(stack.length !== 0){
+//         let {node, depth} = stack.pop();
+//         if(node === null) continue;
+
+//         sumDepth += depth;
+
+//         stack.push({node: node.left, depth: depth + 1})
+//         stack.push({node: node.right, depth: depth + 1})
+//     }
+//     return sumDepth
+// }
