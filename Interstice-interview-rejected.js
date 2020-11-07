f5b6068177e1705f0142c675cef9dd3c9d471908
joexylu <joe.luxiyang@gmail.com>
2020-11-07 12:31:01 -0800

    interview notes

---
+++ Interstice-interview-rejected.js
@@ -0,0 +1,72 @@
+// .App {
+//   text-align: center;
+//   height: 100vh;
+//   width: 100vw;
+//   background-color: rgb(228, 228, 228);
+// }
+
+// .signIn {
+//   height: 70%;
+//   width: 50%;
+//   margin-right: auto;
+//   margin-left: auto;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+// }
+
+// .signIn div{
+//   width: 100%;
+// }
+
+// .email{
+//   height: 20%;
+//   width: 75%;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+// }
+
+// .email label{
+//   right: 0;
+// }
+
+// .password{
+//   height: 20%;
+//   width: 75%;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+// }
+
+// .signInButton{
+//   height: 20%;
+//   width: 75%;
+// }
+
+// .signInButton button{
+//   width: 100%;
+//   height: 75%;
+//   background-color: rgb(0, 187, 0)
+// }
+
+// <div className="App">
+//       <div className="signIn">
+//         <div className="title">
+//           <h1>Welcome to Interstice!</h1>
+//         </div>
+
+//         <div className="email">
+//           <label>Email:</label>
+//           <input type="email"/>
+//         </div>
+
+//         <div className="password">
+//           <label>Password:</label>
+//           <input type="password"/>
+//         </div>
+//         <div className="signInButton">
+//           <button>Sign In</button>
+//         </div>
+//       </div>
+//     </div>
