f9120d3219583072db79f28af9db92db4bafc700
joexylu <joe.luxiyang@gmail.com>
2020-12-03 13:38:26 -0800

    review algo

--- JZ-Offer.js
+++ JZ-Offer.js
@@ -28,7 +28,7 @@ function findShort(arr, target){
 
     return min < Infinity ? min : 0
 }
-// console.log(findShort([2,3,1,2,4,3], 7))
+console.log(findShort([2,3,1,2,4,3], 7))
 
 
 
@@ -137,4 +137,144 @@ function maxPathSum(root){
     dfs(root)
 
     return maxSum
+}
+
+// 矩阵中最长递增路径
+
+function longestIncreasingPath(matrix){
+    if(matrix.length===0) return 0
+
+    const row = matrix.length
+    const col = matrix[0].length
+
+    const memo = new Array(row)
+    for(let i=0; i < row;i++) memo[i] = new Array(col)
+
+    let maxPath = 1
+
+    for(let i=0; i< row; i++){
+        for(let j = 0; j< col; j++){
+            maxPath = Math.max(maxPath, dfsHelper(matrix, i, j, row, col, memo))
+        }
+    }
+
+    return maxPath
+}
+
+function dfsHelper(matrix, x, y, row, col, memo){
+    const arx = [0,1,0,-1]
+    const ary = [1,0,-1,0]
+
+    if(memo[i][j]) return memo[i][j]
+
+    let max = 1
+    for(let i=0;i<4;i++){
+        const xVal = arx[i]+x
+        const yVal = ary[i]+y //找上下左右点
+        if(xVal>=0&&xVal<row&&yVal>=0&&yVal<col&&matrix[xVal][yVal]>matrix[x][y]){
+            max = Math.max(max, 1+dfsHelper(matrix, xVal,yVal,row, col, memo))
+        }
+    }
+    return memo[i][j] = max
+}
+
+// 找二叉树最大深度
+
+function binaryMaxDepth(root){
+    if(!root){
+        return 0
+    }else{
+        const left = binaryMaxDepth(root.left)
+        const right = binaryMaxDepth(root.right)
+        return Math.max(left, right) + 1
+    }
+}
+
+// 合并两个链表
+
+function mergeTwoList(l1,l2){
+    if(!l1) return l2
+    if(!l2) return l1
+
+    var preHead = new ListNode(-1);
+    let pre=preHead
+
+    let list1=l1
+    let list2=l2
+
+    while(list1 && list2){
+        if(list1.val<=list2.val){
+            pre.next=list1
+            list1=list1.next
+        }else{
+            pre.next=list2
+            list2=list2.next
+        }
+        pre=pre.next
+    }
+    pre.next=list1?list1:list2
+    return pre.next
+}
+
+
+//最小堆的实现
+
+class MinHeap{
+    constructor(){
+        this.heap = []
+    }
+
+    swap(i1,i2){
+        [this.heap[i1], this.heap[i2]] = [this.heap[i2], this.heap[i1]]
+    }
+
+    getParentIndex(i){
+        return Math.floor((i-1)/2)
+    }
+
+    getLeftIndex(i){
+        return i * 2 + 1
+    }
+    
+    getRightIndex(i){
+        return i * 2 + 2
+    }
+
+    shiftUp(index){
+        if(index == 0) return
+        const parentIndex = this.getParentIndex(index)
+        if(this.heap[parentIndex] > this.heap[index]){
+            this.swap(parentIndex, index)
+            this.shiftUp(parentIndex)
+        }
+    }
+
+    shiftDown(index){
+        const leftIndex = this.getLeftIndex(index)
+        const rightIndex = this.getRightIndex(index)
+        if(this.heap[leftIndex] < this.heap[index]){
+            this.swap(leftIndex, index)
+            this.shiftDown(leftIndex)
+        }
+        if(this.heap[rightIndex] < this.heap[index]){
+            this.swap(rightIndex, index)
+            this.shiftDown(rightIndex)
+        }
+    }
+
+    insert(value){
+        this.heap.push(value)
+        this.shiftUp(this.heap.length-1)
+    }
+
+    pop(){
+        this.heap[0] = this.heap.pop()
+        this.shiftDown(0)
+    }
+
+    peak(){
+        return this.heap[0]
+    }
+
+
 }
