662db3f06d923c5da58e0acdb44980e0c674047e
joexylu <joe.luxiyang@gmail.com>
2020-12-08 22:50:29 -0800

    deep copy

--- JZ-Offer.js
+++ JZ-Offer.js
@@ -351,3 +351,23 @@ function sum(...args){
     })
 }
 sum().then(()=>())
+
+
+
+// 深拷贝
+function extend(target, source, deep) {
+    for (key in source)
+        if (deep && (isPlainObject(source[key]) || isArray(source[key]))) {
+            // source[key] 是对象，而 target[key] 不是对象， 则 target[key] = {} 初始化一下，否则递归会出错的
+            if (isPlainObject(source[key]) && !isPlainObject(target[key]))
+                target[key] = {}
+
+            // source[key] 是数组，而 target[key] 不是数组，则 target[key] = [] 初始化一下，否则递归会出错的
+            if (isArray(source[key]) && !isArray(target[key]))
+                target[key] = []
+            // 执行递归
+            extend(target[key], source[key], deep)
+        }
+        // 不满足以上条件，说明 source[key] 是一般的值类型，直接赋值给 target 就是了
+        else if (source[key] !== undefined) target[key] = source[key]
+}
