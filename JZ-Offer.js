c15d71a27d0a7cc71623746002fca8f5929f1b61
joexylu <joe.luxiyang@gmail.com>
2020-12-10 17:04:43 -0800

    spiral order matrix

--- JZ-Offer.js
+++ JZ-Offer.js
@@ -406,4 +406,36 @@ var postorder = function(root) {
     return res
   }
 
+// matrix spiral order
+const spiralOrder = (matrix) => {
+    if (matrix.length == 0) return [];
+    const res = [];
   
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+  
+    while (top <= bottom && left <= right) {
+      for (let i = left; i <= right; i++) {
+        res.push(matrix[top][i]);
+      }
+      top++;
+      for (let i = top; i <= bottom; i++) {
+        res.push(matrix[i][right]);
+      }
+      right--;
+  
+      if (top > bottom || left > right) break;
+  
+      for (let i = right; i >= left; i--) {
+        res.push(matrix[bottom][i]);
+      }
+      bottom--;
+      for (let i = bottom; i >= top; i--) {
+        res.push(matrix[i][left]);
+      }
+      left++;
+    }
+    return res;
+  };
